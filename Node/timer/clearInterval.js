9a8386ac63020b349740dfc777c130be22fd4461
Thiago <thiago_murakami@hotmail.com>
2023-05-26 14:41:12 -0300

    add readme and method clearInterval

--- Node/timer/clearInterval.js
+++ Node/timer/clearInterval.js
@@ -1,8 +1,8 @@
 // clearInterval ira cancelar um setInterval registrado
 
-const timeOut = 1000
+const timeOut = 1000 // tempo de preparacao 
 const checking = () => console.log('checking!')
 
 let interval = setInterval(checking, timeOut)
 
-setTimeout( () => clearInterval(interval), 3000)
+setTimeout( () => clearInterval(interval), 3000) // 3000 - 1000 = 2000
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Discover
+- Conteúdos de HTML, CSS, JavaScript, Git e GitHub
+- Projeto para impulsionar o portfólio
+- Quizes e exercícios para fixar seu conhecimento
