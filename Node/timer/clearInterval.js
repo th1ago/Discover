4b0c5669b598e4dd81ac5a5b461464984ddd0caa
Thiago <thiago_murakami@hotmail.com>
2023-05-26 13:54:12 -0300

    adjust folder

---
+++ Node/timer/clearInterval.js
@@ -0,0 +1,8 @@
+// clearInterval ira cancelar um setInterval registrado
+
+const timeOut = 1000
+const checking = () => console.log('checking!')
+
+let interval = setInterval(checking, timeOut)
+
+setTimeout( () => clearInterval(interval), 3000)
