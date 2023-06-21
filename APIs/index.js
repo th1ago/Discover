f7fe926e6304eb2f80d17553a5fe76842ac8f4c8
Thiago <thiago_murakami@hotmail.com>
2023-06-21 18:18:27 -0300

    add post, put and delete

--- APIs/index.js
+++ APIs/index.js
@@ -2,9 +2,30 @@
 const express = require('express')
 // inicializando o express
 const app = express()
+let autor = "Thiago"
 
 app.listen('3000')
 
+// GET
 app.route('/').get((req, res) => {
-    
+    res.send(autor)
+})
+
+// POST
+// ponte entre as requisicoes middleware
+app.use(express.json()) // vou chamar o modulo
+
+app.route('/').post((res, res) => {
+    res.send(req.body)
+})
+
+// PUT
+app.route('/').put((req, res) => {
+    autor = req.body.autor
+    res.send(autor)
+})
+
+// DELETE 
+app.route('/:identificados').delete((req, res) => {
+    res.send(req.params.identificados)
 })
